801e128ad209bce8537c76f505fbcc5eb83c032c
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-04-19 00:17:20 -0400

    moved clean scruipt

--- cleanUp.sh
+++ cleanUp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This removes the executables etc and leaves only source code
+# This kills procecss and removes the executables etc and leaves only source code
+
+kill -9 $(lsof -i:9999 -t) 2> /dev/null
 
 rm -rf trick/
 rm -rf trick.*
--- killServerProcess.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 $(lsof -i:8080 -t) 2> /dev/null
